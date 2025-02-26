206b45e657cb15f729ea8666fd7a9c05c14b8c92
kelvin <kelkipro@gmail.com>
2025-02-26 15:21:06 +0300

    ORM lab

--- lib/debug.py
+++ lib/debug.py
@@ -5,5 +5,21 @@ from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+Department.drop_table()
+Department.create_table()
 
+payroll = Department.create("Payroll", "Building A,5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East wing")
+print(hr)
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)  # <Department 2: HR, Building F, 10th Floor>
+
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
 ipdb.set_trace()
+
